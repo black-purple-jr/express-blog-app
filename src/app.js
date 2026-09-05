ec890a5065c9dd8ed21f10d280518c30de539a12
Abdellah DAKIR ALLAH <dal.dakirallah@gmail.com>
2026-09-05 23:39:48 +0100

    message

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const path = require('path');
 
 // initialize express app
 const app = express();
 
 // register view engine
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 const port = 3000;
