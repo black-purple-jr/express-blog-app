90bf32d02ac4dffa9bcd9ddbdf878ab39f8b1a64
Abdellah DAKIR ALLAH <dal.dakirallah@gmail.com>
2026-09-11 01:03:58 +0100

    update

--- app.js
+++ app.js
@@ -51,7 +51,7 @@ app.post('/blogs', (req, res) => {
 app.get("/blogs/:id", (req, res) => {
   const id = req.params.id;
   Blog.findById(id)
-    .then(result => res.render("details", { blog: result, title: blog.title }))
+    .then(result => res.render("details", { blog: result, title: result.title }))
     .catch(err => console.log(err));
 });
 
